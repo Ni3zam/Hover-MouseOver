70dfab392c6b7fe5600499a62d20063bfbe57571
Ni3zam <rahulniezam2002@gmail.com>
2021-06-10 07:13:34 +0800

    Update onmouse-move.js

--- onmouse-move.js
+++ onmouse-move.js
@@ -1,6 +1,6 @@
 ('button').on('mouseover',function() {
   (this).css({
-   top: (Math.random()*30)+"px",
-   left: (Math.random()*30)+"px", 
+   top: (Math.random()*15)+"px",
+   left: (Math.random()*15)+"px", 
  });
 });
